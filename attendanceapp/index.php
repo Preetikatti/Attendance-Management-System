41ee2bfce531914711bdb50904755b6cba19ed1a
Preetikatti <preetikatti4@gmail.com>
2025-04-12 15:09:39 +0530

    Add files via upload

---
+++ attendanceapp/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Attendance App</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="header-content">
+            
+            <h1>ATTENDANCE MANAGEMENT SYSTEM</h1>
+        </div>
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="email.php">Email</a></li>
+        </ul>
+    </nav>
+    <main>
+        
+    </main>
+    
+    <img src="img1.jpg" width="600" height="400" class="center"> 
+    
+    
+    
+    <footer>
+        <p>&copy; Easy,Fast and Simple Solution</p>
+    </footer>
+</body>
+</html>
